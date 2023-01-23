7ae2e2769af721887d128644389540b5595e5961
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-01-23 09:54:34 -0300

    🐛 Bug fixed on characters page

--- web/src/pages/characters/Characters.tsx
+++ web/src/pages/characters/Characters.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { deleteCharacterRoute, getCharactersRoute } from "../../utils/APIRoutes"
 import { AiFillDelete } from "react-icons/ai"
 import Swal from "sweetalert2"
@@ -8,6 +8,8 @@ import withReactContent from "sweetalert2-react-content"
 
 function Characters() {
     const MySwal = withReactContent(Swal)
+
+    const navigate = useNavigate()
 
     const [characters, setCharacters] = useState<any>([])
 
@@ -55,6 +57,11 @@ function Characters() {
             })
     }
 
+    const navigateToCharacterSheet = (id: string) => {
+        navigate(`/character-sheet/${id}`)
+    }
+
+
     return (
         <div className="pt-12 flex flex-col w-5/6">
             <div className="flex flex-row justify-between w-full" >
@@ -65,23 +72,21 @@ function Characters() {
                 {
                     characters && characters.map((character: any, index: any) => {
                         return (
-                            <Link key={index} to={`/character-sheet/${character.id}`} >
-                                <div className="pt-1 bg-orange-600 rounded-lg w-full overflow-hidden">
-                                    <div className='bg-cloudy px-4 py-4 rounded-t h-full transition hover:bg-rainy' >
-                                        <div className="flex flex-row justify-between pb-2">
-                                            <h1 className="text-white font-semibold text-xl block">{character.name}</h1>
-                                            <button onClick={() => { deleteButton(character) }}>
-                                                <AiFillDelete className="text-orange-600 text-xl hover:text-orange-900" />
-                                            </button>
-                                        </div>
-                                        <div>
-                                            <p className=" text-zinc-500 text-sm">
-                                                Level {character.level} | {character.race ? character.race : "No race selected"} | {character.charClass ? character.charClass : "No class selected"}
-                                            </p>
-                                        </div>
+                            <div className="pt-1 bg-orange-600 rounded-lg w-full overflow-hidden" onClick={() => { navigateToCharacterSheet(character.id) }}>
+                                <div className='bg-cloudy px-4 py-4 rounded-t h-full transition hover:bg-rainy' >
+                                    <div className="flex flex-row justify-between pb-2">
+                                        <h1 className="text-white font-semibold text-xl block">{character.name}</h1>
+                                        <button onClick={(e) => { e.stopPropagation(); deleteButton(character) }}>
+                                            <AiFillDelete className="text-orange-600 text-xl hover:text-orange-900" />
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <p className=" text-zinc-500 text-sm">
+                                            Level {character.level} | {character.race ? character.race : "No race selected"} | {character.charClass ? character.charClass : "No class selected"}
+                                        </p>
                                     </div>
                                 </div>
-                            </Link >
+                            </div>
                         )
                     })
                 }
